5b1b2bbe93d62a774844173ddb9296d433eb5482
Lev Gogol <levgogol@gmail.com>
2020-01-10 22:04:56 +0300

    Update deploy_dockerhub.sh

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,5 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t lev/cicd-buzz:$TAG .
-docker push lev/cicd-buzz
+docker build -f Dockerfile -t $DOCKER_IMAGE:$TAG .
+docker push $DOCKER_IMAGE
